7aded5644f0d6a08a13e210428fe314168b27836
lucasbrsa <lucas.chu.barbosa@gmail.com>
2017-03-08 18:47:37 +1100

    Added updated LeapYear source code

--- leapYearFunction.c
+++ leapYearFunction.c
@@ -9,13 +9,48 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int isLeapYear(int year) {
-	return 1;
-}
+#define TRUE 1
+#define FALSE 0
+#define START_GREG_CAL 1582
+
+int isLeapYear(int year); 
 
 int main(int argc, char *argv[]) {
 
+	int user_year;
+	while (user_year != -1) {
+		printf("Choose a year:\n");
+		scanf("%d", &user_year);
+		
+		if (user_year != -1) {
+
+			assert(user_year > START_GREG_CAL);
+	
+			int leapResult = isLeapYear(user_year);
+
+			if (leapResult == TRUE) {
+				printf("This is a leap year.\n");
+			} else {
+				printf("This is not a leap year.\n");
+			}
+		}
+	}
+
+	printf("Good Bye!\n");
 
 	return EXIT_SUCCESS;
 
+}
+
+int isLeapYear(int year) {
+	// every 400 years is be a leap year
+	if (year % 400 == 0) {
+		return TRUE;
+	}
+	// every century is not a leap year
+	if (year % 4 == 0 && (! year % 100 == 0)) {
+		return TRUE;
+	} else {
+		return FALSE;
+	}
 }
